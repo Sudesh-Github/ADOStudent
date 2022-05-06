29291c9978c786f6faba92e2d89bb9d9bf93a67c
Sudesh K <Sudesh@SudeshK.com>
2022-05-06 13:23:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿//using System;
+//using System.Data;
+//using System.Data.SqlClient;
+
+//namespace ADOStudent
+//{
+//    class Program
+//    {
+//        // Connected Approach for Database Access in ADO.NET
+//        static void Main(string[] args)
+//        {
+//            SqlConnection con1 = new SqlConnection(@"Data Source=DESKTOP-7S4I2CM\SQLEXPRESS; Initial Catalog=Sample; Integrated Security=True;Connect Timeout=30");
+//            con1.Open();
+//            SqlCommand cmd = new SqlCommand();
+//            string str = "select * from Matches";
+//            cmd.CommandText = str;
+//            cmd.Connection = con1;
+
+//            SqlDataReader dr = cmd.ExecuteReader();
+//            Console.WriteLine("Book Records...");
+//            while (dr.Read())
+//            {
+//                int MatchID = Int32.Parse(dr[0].ToString());
+//                string TeamName1 = dr[1].ToString();
+//                Console.WriteLine($"MatchID: {MatchID}, Team Name1: {TeamName1}");
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADOStudent
+{
+    // Disconnected Approach 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con1 = new SqlConnection(@"Data Source=DESKTOP-7S4I2CM\SQLEXPRESS; Initial Catalog=Sample; Integrated Security=True;Connect Timeout=30");
+            con1.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from Matches", con1);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Matches");
+            foreach (DataRow dr in ds.Tables["Matches"].Rows)
+            {
+                int MatchID = Int32.Parse(dr[0].ToString());
+                string Status = dr[4].ToString();
+
+                Console.WriteLine($"MatchID: {MatchID}, WinningTeam: {Status}");
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
